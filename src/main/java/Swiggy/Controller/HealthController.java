421b7a76a98f7a5f915d556c33abdf03bac86bda
lakshay thareja <lakshaythareja92@gmail.com>
2019-06-11 23:21:46 +0530

    Initial commit

--- src/main/java/Swiggy/Controller/HealthController.java
+++ src/main/java/Swiggy/Controller/HealthController.java
@@ -4,11 +4,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController{
     @RequestMapping(value = "/",method = RequestMethod.GET)
+    @ResponseBody
     public ResponseEntity getHealth(){
         return new ResponseEntity(HttpStatus.OK);
     }
